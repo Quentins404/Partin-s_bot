a318a31b42b6cdc56ec30832754e80e91b08d3f3
Quentin <138402819+Quentins404@users.noreply.github.com>
2023-08-31 19:59:04 +0200

    Main script updated

--- main_partins.py
+++ main_partins.py
@@ -10,13 +10,20 @@ from discord import *
 from discord.ext import commands, tasks
 import os
 from blockedWords import BlockedWords
+from token_1 import TOKEN
 from dotenv import load_dotenv
+import re
+
+
+CHANNEL = YOUR_CHANNEL_ID
+
 
 #Load .env file
 load_dotenv()
 # Load .env variables
-discord_token = os.getenv('TOKEN')
+discord_token = os.getenv(TOKEN)
 discord_channel = os.getenv('CHANNEL')
+
 intents = discord.Intents.all()
 intents.message_content = True
 client = commands.Bot(command_prefix="/", intents=discord.Intents.all())
@@ -45,29 +52,43 @@ async def on_member_join(member):
 async def connect(interaction: discord.Interaction):
     embed=discord.Embed(title="**To connect on the server you need to**",description="Start Minecraft \n \n Click on ```Multiplayer```  \n Then on ```Add server``` \n Then paste ```YOUR_SERVER_IP``` \n  in ""server adress"" \n \n  then , click on done \n \n Finally double-click on the server to join it" ,color=discord.Colour.blue())
     await interaction.response.send_message(embed=embed)
+
+
 @client.tree.command(name="help" , description="Gives ypou all the commands")
 async def help(interaction: discord.Interaction):
     embed=discord.Embed(title="**Commands list**",description="/status \n \n /help \n \n /commands \n \n /version \n \n /ping " , color=discord.Colour.yellow())
     await interaction.response.send_message(embed=embed)
+
+
 @client.tree.command(name="ping" , description="Replies with a Pong!")
 async def ping(interaction: discord.Interaction):
     await interaction.response.send_message(f"Pong! {round(client.latency * 1000)}ms")
+
+
 @client.tree.command(name="version" , description="Gives you the version of the bot")
 async def version(interaction: discord.Interaction):
     embed=discord.Embed(title="Version of the bot" , description="Bot version: V.1.10 \n Python version: 3.11.4" , color=discord.Colour.dark_purple())
     await interaction.response.send_message(embed=embed)
     await defer(ephemeral=False , thinking=False)
+
+
 @client.tree.command(name="commands")
 async def commands(interaction: discord.Interaction):
     embed=discord.Embed(title="IG-Commands", description="/sethome homename (sets an home) \n \n /home homename (teleports you to your home) \n \n /hub (teleports you to the hub)")
     await interaction.response.send_message(embed=embed)
+
+
 @client.tree.command(name="status", description="gives you the status of the server")
 async def status(interaction: discord.Interaction):
         embed=discord.Embed(title="**I'm checking the status...**",description="The server is ON :green_square: \n \n To connect , just go on the #How-to-connect channel." , color=discord.Colour.green())
         await interaction.response.send_message(embed=embed)
         await defer(ephemeral=False, thinking=False)
+
+
 # Don't forget to add role to your server
-@client.tree.command(name="muted", description="add role 'mute' to someone")
+
+
+@client.tree.command(name="mute", description="add role 'mute' to someone")
 async def mute(ctx, member: discord.Member):
         # Rôle "Muted"
         muted_role = discord.utils.get(ctx.guild.roles, name="Muted")
@@ -79,10 +100,31 @@ async def mute(ctx, member: discord.Member):
         # Add "Muted" to member
         await member.add_roles(muted_role)
         print((f"{member.mention} a été muté."))
-# TODO Add a "unmute" command
+        channel = client.get_channel(discord_channel)
+        await channel.send("User Muted")
+
+
+# TODO Add a "unmute" command , Find a way to make the bot send a message when the command is send.
+
+
 @client.event
 async def on_message(ctx):
     print(f"{ctx.channel}: {ctx.author}: {ctx.author.name}: {ctx.content}")
     if any(x in ctx.content for x in BlockedWords):
         await ctx.delete()
-client.run(discord_token)
+
+
+#Makes the bot respond to a hello message where he is mentioned.
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+  
+    pattern = re.compile(f"hello <@!?{client.user.id}>") # The exclamation mark is optional
+
+    if pattern.match(message.content.lower()) is not None: # Checking whether the message matches our pattern
+        await message.channel.send(f"Hello {message.author.mention}!")
+
+
+client.run(TOKEN)
