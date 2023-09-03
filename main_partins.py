071bd957f44e5bd3d47987bf38825c8858f8e842
Quentin <138402819+Quentins404@users.noreply.github.com>
2023-09-03 14:28:17 +0200

    Fixed client.event bugs
    ADDED
    -Buttons (Ping command and meme command)
    -Replaced all the client.event by client.listen and f1,2,3...
    -All code is now commented
    
    FIXED
    -Join message
    -Blockedwords messages
    -mute/unmute commands

--- main_partins.py
+++ main_partins.py
@@ -1,3 +1,4 @@
+#imports
 import random
 import colorsys
 import discord
@@ -5,32 +6,29 @@ import defer
 from discord import app_commands
 from discord import embeds
 from discord import colour
-from discord.ext import commands
 from discord import *
+from discord.ext import *
 from discord.ext import commands, tasks
+from discord import Client, Intents
+from blockedWords import blockedWords
 import os
-from blockedWords import BlockedWords
+import time
 from token_1 import TOKEN
-from dotenv import load_dotenv
-import re
 import pyrandmeme
 from pyrandmeme import *
 import requests
+import re
 
 
-#Load .env file
-load_dotenv()
-# Load .env variables
-discord_token = os.getenv(TOKEN)
-discord_channel = os.getenv('CHANNEL')
 
+
+#Bot Setup
 intents = discord.Intents.all()
-intents.message_content = True
 client = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 
-# Events
-@client.event
-async def on_message(message):
+
+@client.listen('on_message')
+async def f4(message):
     print(f"{str(message.author)} in {message.guild.name}: {message.content}")
 
 @client.event
@@ -43,16 +41,18 @@ async def on_ready():
     except Exception as e:
         print(e)    
 
-@client.event
-async def on_member_join(member):
-    channel = client.get_channel(discord_channel)
+
+#join message
+@client.listen('on_member_join')
+async def f3(member):
+    channel = client.get_channel(1139570392973975553)
     embed=discord.Embed(title="Welcome!",description=f"{member.mention}just arrived!" , color=discord.Colour.green())
     await channel.send(embed=embed)
 
 # Commands 
 @client.tree.command(name="connect")
 async def connect(interaction: discord.Interaction):
-    embed=discord.Embed(title="**To connect on the server you need to**",description="Start Minecraft \n \n Click on ```Multiplayer```  \n Then on ```Add server``` \n Then paste ```YOUR_SERVER_IP``` \n  in ""server adress"" \n \n  then , click on done \n \n Finally double-click on the server to join it" ,color=discord.Colour.blue())
+    embed=discord.Embed(title="**To connect on the server you need to**",description="Start Minecraft \n \n Click on ```Multiplayer```  \n Then on ```Add server``` \n Then paste ``` 172.99.189.127:35600``` \n  in ""server adress"" \n \n  then , click on done \n \n Finally double-click on the server to join it" ,color=discord.Colour.blue())
     await interaction.response.send_message(embed=embed)
 
 
@@ -64,7 +64,7 @@ async def help(interaction: discord.Interaction):
 
 @client.tree.command(name="ping" , description="Replies with a Pong!")
 async def ping(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Pong! {round(client.latency * 1000)}ms")
+    await interaction.response.send_message(view=MyView2())
 
 
 @client.tree.command(name="version" , description="Gives you the version of the bot")
@@ -109,7 +109,7 @@ async def mute(interaction: discord.Interaction, member: discord.Member):
 
     # Send a DM to the muted user
     embed = discord.Embed(title="You have been Muted", description="You have been muted in the server.")
-    await interaction.user.send(embed=embed)
+    await member.send(embed=embed)
 
     # Send a message using interaction response
     response_embed = discord.Embed(title="User Muted", description=f"{member.mention} has been muted :white_check_mark:" , color=discord.Colour.red())
@@ -130,17 +130,17 @@ async def mute(interaction: discord.Interaction, member: discord.Member):
         # Create the "Muted" role with necessary permissions
         muted_role = await interaction.guild.create_role(name="Muted", permissions=discord.Permissions(send_messages=False, speak=False))
 
-        # Apply the "Muted" role to the default text channels (optional)
+        # Apply the "unMuted" role to the default text channels (optional)
         for channel in interaction.guild.text_channels:
             await channel.set_permissions(muted_role, send_messages=False)
 
-    # Add the "Muted" role to the member
+    # Add the "unMuted" role to the member
     await member.remove_roles(muted_role)
     print(f"{member.mention} has been unmuted.")
 
-    # Send a DM to the muted user
+    # Send a DM to the unmuted user
     embed = discord.Embed(title="You have been unmuted", description="You have been Unmuted in the server.")
-    await interaction.user.send(embed=embed)
+    await member.send(embed=embed)
 
     # Send a message using interaction response
     response_embed = discord.Embed(title="User Unmuted", description=f"{member.mention} has been Unmuted :white_check_mark:" , color=discord.Colour.green())
@@ -149,17 +149,23 @@ async def mute(interaction: discord.Interaction, member: discord.Member):
     # Remove the "Newbie" role (optional)
     await member.add_roles(newbie)
 
-@client.event
-async def on_message(ctx):
-    print(f"{ctx.channel}: {ctx.author}: {ctx.author.name}: {ctx.content}")
-    if any(x in ctx.content for x in BlockedWords):
-        await ctx.delete()
 
+@client.tree.context_menu(name="vocalmute")
+async def mute2(interaction:discord.Interaction, member:discord.Member):
+    await member.edit(mute=True)
+    await interaction.response.send_message(f"{member} has been muted")
 
-#Makes the bot respond to a hello message where he is mentioned.
+#events
+#reminder to use listen and f everytime
 
-@client.event
-async def on_message(message):
+@client.listen('on_message')
+async def f2(message:discord.Message): 
+    if any(x in message.content for x in blockedWords):
+        await message.delete()
+    client.process_commands(message)
+
+@client.listen('on_message')
+async def f1(message:discord.Message):
     if message.author == client.user:
         return
   
@@ -168,12 +174,16 @@ async def on_message(message):
     if pattern.match(message.content.lower()) is not None: # Checking whether the message matches our pattern
         await message.channel.send(f"**I am going to implode.** {message.author.mention}" , reference=message) 
 
-        
+
+
+
+
+#commands
 
 @client.tree.command(name="random" , description="Gives you a random number beetween 1 and 1000000")
 async def random(interaction: discord.Interaction):
     import random
-    await interaction.response.send_message(random.randint(0, 1000000))
+    await interaction.response.send_message(view=MyView2())
 
 @client.tree.command(name="express_rage" , description="EXPRESS YOUR RAGE")
 async def express_rage(interaction: discord.Interaction):
@@ -182,9 +192,29 @@ async def express_rage(interaction: discord.Interaction):
 
 @client.tree.command(name="meme" , description="Send a random meme")
 async def meme(interaction:discord.Interaction):
-    await interaction.response.send_message(embed=await pyrandmeme())
+    await interaction.response.send_message(view=MyView())
+
+@client.tree.command(name="rules")
+async def rules(interaction:discord.Interaction):
+    embed=discord.Embed(title="Rules", description="https://docs.google.com/document/d/14fmVt4_eU3QesQVC8VfkSKL0i4EB5hQVI1c3RRCUUOE/edit?usp=sharing" , color=discord.Colour.dark_blue())
+    await interaction.response.send_message(embed=embed)
+
+#buttons
+
+class MyView(discord.ui.View): # Create a class called MyView that subclasses discord.ui.View
+    @discord.ui.button(label="Click me to generate a meme !", style=discord.ButtonStyle.primary, emoji="🤣") # Create a button with the label "😎 Click me!" with color Blurple
+    async def button_callback(self , interaction , button):
+        await interaction.response.send_message(embed=await pyrandmeme()) # Send a message when the button is clicked
+
+class MyView2(discord.ui.View): # Create a class called MyView that subclasses discord.ui.View
+    @discord.ui.button(label="Click me to check the ping", style=discord.ButtonStyle.success) # Create a button with the label "😎 Click me!" with color Blurple
+    async def button_callback(self , interaction , button):
+        await interaction.response.send_message(f"Pong! {round(client.latency * 1000)}ms") # Send a message when the button is clicked
 
 
 
+
+
+#TOKEN
 
 client.run(TOKEN)
